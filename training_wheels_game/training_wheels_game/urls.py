30aae5ed9587e4571adfba5ced47c53dbbcc9ea1
Michal Wroblewski <mwroblewski@i2asolutions.com>
2019-01-09 12:18:31 +0100

    Added SignUp page

--- training_wheels_game/training_wheels_game/urls.py
+++ training_wheels_game/training_wheels_game/urls.py
@@ -21,6 +21,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('training_wheels_game/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
